8586f67e9f36c7a18cf5e921c23ec1dcc9c25945
Adrián Catalán <adrian.catalan@travel-ims.com>
2019-05-05 10:28:32 +0200

    Add 05-basics

---
+++ 05-basics/HelloWorld.py
@@ -0,0 +1,16 @@
+__author__= 'adr'
+print('Hello, World!')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The End")
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+
+greeting = "Hello"
+name = "Bruce"
+
+print(greeting + name)
+# This is a comment
+print(greeting +  ' ' +  name)
