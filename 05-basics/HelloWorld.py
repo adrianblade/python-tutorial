588e401b0d867e0ec24bc74cb44f892661f4ced6
Adrian Catalan <adrian.catalan@travel-ims.com>
2019-05-05 11:38:36 +0200

    Add String formatting

--- 05-basics/HelloWorld.py
+++ 05-basics/HelloWorld.py
@@ -14,3 +14,25 @@ name = "Bruce"
 print(greeting + name)
 # This is a comment
 print(greeting +  ' ' +  name)
+
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + ' ' + name)
+
+splitString = "This string has been\nsplit over\nseveral"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t"
+print(tabbedString)
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
+
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[1])
+print(parrot[0:6])
+print(parrot[:6])
+print(parrot[6:])
